c350f6a3259f6040ebe59ec5c2a08bc6ee5e22c4
Sigve Hansen <sigve@Sigves-MacBook-Air.local>
2020-06-20 16:49:33 +0200

    change from sass to css-variables

--- src/component.vue
+++ src/component.vue
@@ -2,20 +2,36 @@
   <div class="elder__croppie" :style="background">
     <div class="elder__croppie-actions" v-if="value || instance">
       <template v-if="!instance">
-        <button type="button" class="elder__croppie-button elder__croppie-button--error" @click="reset">
+        <button
+          type="button"
+          class="elder__croppie-button elder__croppie-button--error"
+          @click="reset"
+        >
           <fa icon="times"></fa>
         </button>
-        <button type="button" class="elder__croppie-button" @click="$refs.file.click()">
+        <button
+          type="button"
+          class="elder__croppie-button"
+          @click="$refs.file.click()"
+        >
           <fa icon="pencil-alt"></fa>
         </button>
         <slot name="view-actions"></slot>
       </template>
       <template v-else>
         <template v-if="canRotate">
-          <button type="button" class="elder__croppie-button" @click="rotate(90)">
+          <button
+            type="button"
+            class="elder__croppie-button"
+            @click="rotate(90)"
+          >
             <fa icon="undo"></fa>
           </button>
-          <button type="button" class="elder__croppie-button" @click="rotate(-90)">
+          <button
+            type="button"
+            class="elder__croppie-button"
+            @click="rotate(-90)"
+          >
             <fa icon="undo" flip="horizontal"></fa>
           </button>
         </template>
@@ -23,26 +39,38 @@
       </template>
     </div>
     <div v-if="showPlaceholder" class="elder__croppie-placeholder">
-      <button-component type="button" primary :label="placeholder" icon="plus" @click="$refs.file.click()"/>
+      <button-component
+        type="button"
+        primary
+        :label="placeholder"
+        icon="plus"
+        @click="$refs.file.click()"
+      />
     </div>
     <div ref="croppie"></div>
-    <input ref="file" type="file" accept="image/*" class="elder__croppie-upload" @change="prepare($event)" />
+    <input
+      ref="file"
+      type="file"
+      accept="image/*"
+      class="elder__croppie-upload"
+      @change="prepare($event)"
+    />
   </div>
 </template>
 
 <script>
-import { Croppie } from 'croppie'
-import 'croppie/croppie.css'
-import './icons'
-import { ButtonComponent } from 'vue-elder-button'
-import { FontAwesomeIcon as Fa } from '@fortawesome/vue-fontawesome'
+import { Croppie } from "croppie";
+import "croppie/croppie.css";
+import "./icons";
+import { ButtonComponent } from "vue-elder-button";
+import { FontAwesomeIcon as Fa } from "@fortawesome/vue-fontawesome";
 
 export default {
   props: {
     value: [String, File, Blob],
     placeholder: {
       type: String,
-      default: 'Last opp bilde',
+      default: "Last opp bilde",
     },
     options: Object,
   },
@@ -51,87 +79,91 @@ export default {
       parsedValue: null,
       processing: false,
       instance: null,
-    }
+    };
   },
   watch: {
     value: {
       handler() {
-        this.parseValue()
+        this.parseValue();
       },
       immediate: true,
     },
   },
   computed: {
     background() {
-      if (!this.parsedValue || this.instance) return null
-      return { backgroundImage: `url(${this.parsedValue})` }
+      if (!this.parsedValue || this.instance) return null;
+      return { backgroundImage: `url(${this.parsedValue})` };
     },
     showPlaceholder() {
-      return !this.instance && !this.value
+      return !this.instance && !this.value;
     },
     canRotate() {
-      return this.options && this.options.enableOrientation
+      return this.options && this.options.enableOrientation;
     },
   },
   methods: {
     parseValue() {
-      if (!this.value || typeof this.value === 'string') return (this.parsedValue = this.value)
-      this.processing = true
+      if (!this.value || typeof this.value === "string")
+        return (this.parsedValue = this.value);
+      this.processing = true;
 
-      let reader = new FileReader()
+      let reader = new FileReader();
       reader.onload = () => {
-        this.parsedValue = reader.result
-        this.processing = false
-      }
-      reader.readAsDataURL(this.value)
+        this.parsedValue = reader.result;
+        this.processing = false;
+      };
+      reader.readAsDataURL(this.value);
     },
     reset() {
-      this.$emit('input', null)
+      this.$emit("input", null);
     },
     init(src) {
-      this.processing = true
-      this.destroy()
+      this.processing = true;
+      this.destroy();
 
-      this.instance = new Croppie(this.$refs.croppie, this.options)
+      this.instance = new Croppie(this.$refs.croppie, this.options);
 
       this.instance
         .bind({
           url: src,
         })
-        .then(() => (this.processing = false))
+        .then(() => (this.processing = false));
 
-      this.$emit('ready', this.instance)
+      this.$emit("ready", this.instance);
     },
     rotate(deg) {
-      if (!this.instance) return
-      this.instance.rotate(deg)
+      if (!this.instance) return;
+      this.instance.rotate(deg);
     },
     prepare(e) {
-      if (!e.target.files.length) return
+      if (!e.target.files.length) return;
 
-      let reader = new FileReader()
+      let reader = new FileReader();
       reader.onload = () => {
-        this.init(reader.result)
-      }
-      reader.readAsDataURL(e.target.files[0])
+        this.init(reader.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
     },
     destroy() {
-      if (!this.instance) return
+      if (!this.instance) return;
 
-      this.instance.destroy()
+      this.instance.destroy();
     },
   },
   components: {
     Fa,
     ButtonComponent,
   },
-}
+};
 </script>
 
 <style lang="scss">
-@import '~node_modules/vue-elder-defaults/styles/variables';
+@import "./main";
 
-$vue-elder-croppie--background: #eaeaea !default;
+:root {
+  @include GenerateVariables();
+  @include GenerateVariable("croppie-background", #eaeaea);
+}
 
 .elder__croppie {
   width: 100%;
@@ -141,7 +173,7 @@ $vue-elder-croppie--background: #eaeaea !default;
   justify-content: center;
   align-items: center;
 
-  background-color: $vue-elder-croppie--background;
+  background-color: var(--vue-elder-croppie-background);
   background-position: center;
   background-repeat: no-repeat;
   background-size: contain;
@@ -156,11 +188,11 @@ $vue-elder-croppie--background: #eaeaea !default;
 
   &-button {
     $size: 30px;
-    background-color: $primary;
-    color: $primary-contrast;
+    background-color: var(--vue-elder-primary);
+    color: var(--vue-elder-primary-contrast);
     border: none;
     outline: none;
-    border-radius: $border-radius;
+    border-radius: var(--vue-elder-border-radius);
     font: inherit;
 
     display: flex;
@@ -171,8 +203,8 @@ $vue-elder-croppie--background: #eaeaea !default;
     height: $size;
 
     &--error {
-      background-color: $error;
-      color: $error-contrast;
+      background-color: var(--vue-elder-error);
+      color: var(--vue-elder-error-contrast);
     }
 
     & + & {
@@ -205,4 +237,3 @@ $vue-elder-croppie--background: #eaeaea !default;
   }
 }
 </style>
-
